2f9ec486b4c9ead10b1c91c601e73635036596b5
srodrigo <sergio.rodrigo.royo@gmail.com>
2013-11-18 23:26:15 +0100

    Removed ".." form includes. Used base src directory and fixed premake4 script.

--- src/test/test.h
+++ src/test/test.h
@@ -1,11 +1,9 @@
 #ifndef _TEST_CONFIG_H_
 #define _TEST_CONFIG_H_
 
-#include "../lge/lge.h"
+#include <string>
 
 #include <unittest++/UnitTest++.h>
-#include <stdio.h>
-#include <string>
 
 const std::string TEST_IMAGE_1 = "../../res/test/tux1.jpg";
 const std::string TEST_IMAGE_2 = "../../res/test/tux2.jpg";
